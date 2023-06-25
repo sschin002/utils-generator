4258f10e7beb25d16ec75d4123849c7cfcc2badf
Sachin <sschin002@gmail.com>
2023-06-25 14:55:57 +0545

    converter through ui

--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const CC = require("currency-converter-lt");
 
 router.get("/", (req, res) => {
   res.send("Hello from UI");
 });
 
-// router.get("/converter/:currency1/:currency2/:value", (req, res) => {
-//     const {currency}
-// });
+router.get("/converter/:currency1/:currency2/:value", async (req, res) => {
+  const { currency1, currency2, value } = req.params;
+  const currencyConverter = new CC({
+    from: currency1,
+    to: currency2,
+    amount: Number(value),
+  });
+
+  const result = await currencyConverter.convert();
+  res.send(`${result}`);
+});
 
 module.exports = router;
