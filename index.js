d594f4878a958f362a1f4065d2462fd42d5e93b3
Sachin <sschin002@gmail.com>
2023-06-27 13:59:25 +0545

    morgan

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const indexRouter = require("./routes");
 
+app.use(morgan("tiny"));
 app.use("/", indexRouter);
 
 app.listen(8000, () => {
