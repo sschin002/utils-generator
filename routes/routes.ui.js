a06c61474c59ad1a839c1463ede3d9ee57a36fb2
Sachin <sschin002@gmail.com>
2023-06-27 14:22:50 +0545

    single pager website created

--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const CC = require("currency-converter-lt");
 
 router.get("/", (req, res) => {
-  res.send("Hello from UI");
+  res.render("index");
 });
 
 router.get("/converter/:currency1/:currency2/:value", async (req, res) => {
