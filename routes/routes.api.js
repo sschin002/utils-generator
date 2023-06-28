7dd7c0e780c484dde43383b3cba69f03fe3307b2
Sachin <sschin002@gmail.com>
2023-06-28 14:29:03 +0545

    route level error handling

--- routes/routes.api.js
+++ routes/routes.api.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ msg: "Hello from API" });
+router.get("/", (req, res, next) => {
+  try {
+    res.json({ msg: "Hello from API" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
